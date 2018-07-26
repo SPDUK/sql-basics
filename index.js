c7b782c407f39a8a32e46384896ab331818000dc
SPDUK <33531423+SPDUK@users.noreply.github.com>
2018-07-26 01:17:24 +0100

    added in sequelize, set up the database and .env + put some data in

--- index.js
+++ index.js
@@ -1,8 +1,36 @@
+require('dotenv').config();
+
 const express = require('express');
+
 const faker = require('faker');
 const app = express();
+var Sequelize = require('sequelize');
 
-app.get('/', (req, res) => res.send('Hello World!'));
+var sq = new Sequelize('sqlbasics', process.env.DB_USER, process.env.DB_PW, {
+  host: 'localhost',
+  dialect: 'postgres',
 
-console.log(faker.internet.email());
+  pool: {
+    max: 5,
+    min: 0,
+    idle: 10000
+  }
+});
+
+const User = sq.define('user', {
+  username: Sequelize.STRING,
+  birthday: Sequelize.DATE
+});
+sq.sync()
+  .then(() =>
+    User.create({
+      username: 'janedoe',
+      birthday: new Date(1980, 6, 20)
+    }).catch(err => console.log(err))
+  )
+  .then(jane => {
+    console.log(jane.toJSON());
+  })
+  .catch(err => console.log(err));
+
 app.listen(3000, () => console.log('Example app listening on port 3000!'));
