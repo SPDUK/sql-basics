6f29291b18a87d25ea9e964619c9ac45dcca047d
SPDUK <33531423+SPDUK@users.noreply.github.com>
2018-07-26 23:22:29 +0100

    made the api call work in react

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,31 @@
 import React, { Component } from 'react';
+import axios from 'axios';
+
 import logo from './logo.svg';
 import './App.css';
 
+const hi = 'hello';
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      name: {}
+    };
+  }
+  componentDidMount() {
+    this.fetchApi();
+  }
+
+  async fetchApi() {
+    return await axios
+      .get('/api/users')
+      .then(res => this.setState({ name: res.data }));
+  }
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1>{this.state.name.id}</h1>
+        <h1>{this.state.name.username}</h1>
       </div>
     );
   }
