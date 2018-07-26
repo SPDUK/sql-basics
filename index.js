2b13040e91481af3cdb55efda426594b90868b6b
SPDUK <33531423+SPDUK@users.noreply.github.com>
2018-07-26 19:51:41 +0100

    .

--- index.js
+++ index.js
@@ -53,4 +53,6 @@ app.set('view engine', 'html');
 //   });
 // });
 
-app.listen(3000, () => console.log('Example app listening on port 3000!'));
+app.listen(process.env.PORT || 3000, () =>
+  console.log('Example app listening on port 3000!')
+);
