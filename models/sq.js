5442b05296fe88886bfd168bba72d856257fe5eb
SPDUK <33531423+SPDUK@users.noreply.github.com>
2018-07-27 21:56:18 +0100

    fixed console warning message

--- models/sq.js
+++ models/sq.js
@@ -4,6 +4,8 @@ const sq = new Sequelize('sqlbasics', process.env.DB_USER, process.env.DB_PW, {
   host: 'localhost',
   dialect: 'postgres',
   port: 5432,
+  // to stop deprication warning in console
+  operatorsAliases: false,
 
   pool: {
     max: 5,
