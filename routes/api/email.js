0d3f4439ec9d2a24693460049dae829ee4107717
SPDUK <spdevuk@gmail.com>
2018-08-03 06:27:08 +0100

    Added in update for user to confirm the email

--- routes/api/email.js
+++ routes/api/email.js
@@ -4,8 +4,10 @@ const express = require('express');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 
-const router = express.Router();
+const sq = require('../../models/sq');
+const User = require('../../models/User');
 
+const router = express.Router();
 function sendEmail(
   parentCallback,
   fromEmail,
@@ -57,41 +59,62 @@ function sendEmail(
 
 // TODO: import sq and user etc
 
+// set up a JWT that will expire after 1 hour
 router.post('/', (req, res) => {
-  // jwt
-
-  console.log(res.body);
-  async.parallel(
-    [
-      function(callback) {
-        sendEmail(
-          callback,
-          'noreply@fira.app',
-          ['spdevuk@gmail.com'],
-          'Subject Line',
-          'Text Content',
-          `<a href="http://localhost:8888/api/confirm/${hash}"> <p style="font-size: 32px;">Please click this link to verify your <strong>account</strong></p></a>`
-        );
+  // console.log(req.body);
+  jwt.sign(
+    {
+      data: req.body.id
+    },
+    process.env.EMAIL_SECRET,
+    { expiresIn: '1h' },
+    (err, emailToken) => {
+      const url = `https://localhost:8888/api/confirm/${emailToken}`;
+      async.parallel(
+        [
+          function(callback) {
+            sendEmail(
+              callback,
+              'noreply@fira.app',
+              ['spdevuk@gmail.com'],
+              'Subject Line',
+              'Text Content',
+              `<a href="${url}"> <p style="font-size: 32px;">Please click this link to verify your <strong>account</strong></p>${url}</a>`
+            );
+          }
+        ],
+        (err, results) => {
+          if (err) console.log(err);
+          res.send({
+            success: true,
+            message: 'Emails sent',
+            successfulEmails: results[0].successfulEmails,
+            errorEmails: results[0].errorEmails
+          });
+        }
+      );
+      if (err) {
+        return res.status(404).json(err);
       }
-    ],
-    (err, results) => {
-      if (err) console.log(err);
-      res.send({
-        success: true,
-        message: 'Emails sent',
-        successfulEmails: results[0].successfulEmails,
-        errorEmails: results[0].errorEmails
-      });
     }
   );
 });
+// create a token from the id
+// send that token to the id
+// decrypt it in the get route, if they match then verify the user in db
 
 // using a wildcard because bcrypt strings come with / in it and I don't know how to remove it
 // TODO: remove / from bcrypt or something.
-router.get('/*', (req, res) => {
+router.get('/:id', (req, res) => {
   res.send('hi');
-  console.log(req.params);
-  console.log('route hit');
+  jwt.verify(req.params.id, process.env.EMAIL_SECRET, (err, decoded) => {
+    console.log(decoded);
+    User.update({ confirmed: true }, { where: { id: decoded.data } }).then(
+      user => {
+        console.log(user);
+      }
+    );
+  });
 });
 
 module.exports = router;
